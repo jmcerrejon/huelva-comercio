95a19dcaa0767232d3dfef8ac4ca730cc2379308
Jose Cerrejon <ulysess@gmail.com>
2021-02-03 14:04:00 +0100

    👌 IMPROVE: Just for test purpose I set commented setBool IsConnected = false

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -1,11 +1,8 @@
 (function constructor(args) {
-    if (Ti.App.Properties.getBool("isConnected")) {
-        Alloy.createController("dashboard")
-            .getView()
-            .open();
+    // Ti.App.Properties.setBool('isConnected', false);
+    if (Ti.App.Properties.getBool('isConnected')) {
+        Alloy.createController('dashboard').getView().open();
     } else {
-        Alloy.createController("login/login", args)
-            .getView()
-            .open();
+        Alloy.createController('login/login', args).getView().open();
     }
 })($.args);

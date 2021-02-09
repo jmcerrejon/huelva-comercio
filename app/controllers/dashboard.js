0eb45facfb085ada6c0b3ea23289d55925e53d95
Jose Cerrejon <ulysess@gmail.com>
2021-02-09 10:28:27 +0100

    👌 IMPROVE: Refactor code

--- app/controllers/dashboard.js
+++ app/controllers/dashboard.js
@@ -4,142 +4,155 @@ var fcm = require('firebase.cloudmessaging');
 let timeSinceBackgroundApp;
 const time2RefreshNewsAndEventsInMinutes = 30;
 
-const tabStacks = [{
-	'tabId': 'main',
-	'winId': 'mainCtrl',
-	'winName': 'Inicio',
-	'hasSettingsEnabled': true
-}, {
-	'tabId': 'events',
-	'winId': 'eventsCtrl',
-	'winName': 'Agenda',
-	'hasSettingsEnabled': false
-}, {
-	'tabId': 'affiliates',
-	'winId': 'affiliatesCtrl',
-	'winName': 'Asociaciones',
-	'hasSettingsEnabled': false
-}, {
-	'tabId': 'offers',
-	'winId': 'offersCtrl',
-	'winName': 'Demanda',
-	'hasSettingsEnabled': false
-}, {
-	'tabId': 'contact',
-	'winId': 'contactCtrl',
-	'winName': 'Contacto',
-	'hasSettingsEnabled': false
-}, ];
+const tabStacks = [
+    {
+        tabId: 'main',
+        winId: 'mainCtrl',
+        winName: 'Principal',
+        hasSettingsEnabled: true,
+    },
+    {
+        tabId: 'associations',
+        winId: 'associationsCtrl',
+        winName: 'Asociaciones',
+        hasSettingsEnabled: false,
+    },
+    {
+        tabId: 'offers',
+        winId: 'offersCtrl',
+        winName: 'Demanda',
+        hasSettingsEnabled: false,
+    },
+    {
+        tabId: 'contact',
+        winId: 'contactCtrl',
+        winName: 'Contacto',
+        hasSettingsEnabled: false,
+    },
+];
+
+if (Alloy.Globals.isAffiliate) {
+    tabStacks.splice(1, 0, {
+        tabId: 'affiliates',
+        winId: 'affiliatesCtrl',
+        winName: 'Afiliados',
+        hasSettingsEnabled: false,
+    });
+}
 
 let oldTab = 'main';
 
 (function constructor() {
-	(OS_ANDROID) ? $.tabGroup.addEventListener('androidback', function () {}) : $.tabGroup.hideNavBar();
+    OS_ANDROID
+        ? $.tabGroup.addEventListener('androidback', function () {})
+        : $.tabGroup.hideNavBar();
 
-	Alloy.Globals.events.off('popToRootWindow');
-	Alloy.Globals.events.off('openWindowInTab');
-	Alloy.Globals.affiliatesWin = $.nwAffiliates;
-	Alloy.Globals.offersWin = $.offers;
-	if (OS_IOS) {
-		$.nwAffiliates.hideNavBar();
-	}
+    Alloy.Globals.events.off('popToRootWindow');
+    Alloy.Globals.events.off('openWindowInTab');
+    Alloy.Globals.affiliatesWin = $.nwAffiliates;
+    Alloy.Globals.offersWin = $.offers;
+    // if (OS_IOS) {
+    // $.nwAffiliates.hideNavBar();
+    // }
 })();
 
 if (OS_ANDROID) {
-	$.tabGroup.addEventListener('open', () => {
-		(_.isNull(Alloy.Globals.androidDataPush)) || notification(Alloy.Globals.androidDataPush)
-	});
+    $.tabGroup.addEventListener('open', () => {
+        _.isNull(Alloy.Globals.androidDataPush) ||
+            notification(Alloy.Globals.androidDataPush);
+    });
 }
 
 function closeToRoot() {
-	$.tabGroup.activeTab.popToRootWindow();
+    $.tabGroup.activeTab.popToRootWindow();
 }
 
 Alloy.Globals.events.on('popToRootWindow', closeToRoot);
 
 function focusWindow(e) {
-	switch (e.source.id) {
-		case 'offersCtrl':
-			Alloy.Globals.events.trigger('offers');
-			break;
+    switch (e.source.id) {
+        case 'offersCtrl':
+            Alloy.Globals.events.trigger('offers');
+            break;
 
-		case 'affiliatesCtrl':
-			Alloy.Globals.events.trigger('associations');
-			break;
-	}
+        case 'affiliatesCtrl':
+            Alloy.Globals.events.trigger('associations');
+            break;
+    }
 }
 
 function openWindow(o) {
-	const tab = $.tabGroup.activeTab;
+    const tab = $.tabGroup.activeTab;
 
-	_.defaults(o, {
-		data: {},
-		controller: null,
-		dispatcher: null
-	});
+    _.defaults(o, {
+        data: {},
+        controller: null,
+        dispatcher: null,
+    });
 
-	let win = Alloy.createController(o.controller, o.data);
-	let currentWin = win.getView();
+    let win = Alloy.createController(o.controller, o.data);
+    let currentWin = win.getView();
 
-	function close() {
-		if (currentWin) {
-			tab.close(currentWin);
-			currentWin = null;
-		}
-		if (win) {
-			win.off('close');
-		}
-		win = null;
-	}
-	win.on('close', close);
-	win.on('select', function (e) {
-		if (o.dispatcher) {
-			Alloy.Globals.events.trigger(o.dispatcher, e);
-		}
+    function close() {
+        if (currentWin) {
+            tab.close(currentWin);
+            currentWin = null;
+        }
+        if (win) {
+            win.off('close');
+        }
+        win = null;
+    }
+    win.on('close', close);
+    win.on('select', function (e) {
+        if (o.dispatcher) {
+            Alloy.Globals.events.trigger(o.dispatcher, e);
+        }
 
-		close();
-	});
+        close();
+    });
 
-	if (currentController !== o.controller) {
-		tab.open(currentWin);
-	}
-	if (o.controller === 'win') {
-		currentController = o.data.controller;
-	} else {
-		currentController = o.controller;
-	}
+    if (currentController !== o.controller) {
+        tab.open(currentWin);
+    }
+    if (o.controller === 'win') {
+        currentController = o.data.controller;
+    } else {
+        currentController = o.controller;
+    }
 }
 
 Alloy.Globals.events.on('openWindowInTab', openWindow);
 
 function changeTab(e) {
-	if (e.source.idMenu === 'logout') {
-		logout();
-	} else {
-		$.tabGroup.activeTab = objTab[e.source.idMenu].getView();
-	}
+    if (e.source.idMenu === 'logout') {
+        logout();
+    } else {
+        $.tabGroup.activeTab = objTab[e.source.idMenu].getView();
+    }
 }
 
 function focus(e) {
-	if (e.tab && tabStacks[e.index].tabId.indexOf(e.tab.id) > -1) {
-		$[`${oldTab}Content`].opacity = 0;
-		oldTab = e.tab.id;
-		$[`${e.tab.id}Content`].animate({
-			opacity: 1,
-			duration: 300
-		});
-		changeNavBar(e.index);
-	}
+    if (e.tab && tabStacks[e.index].tabId.indexOf(e.tab.id) > -1) {
+        $[`${oldTab}Content`].opacity = 0;
+        oldTab = e.tab.id;
+        $[`${e.tab.id}Content`].animate({
+            opacity: 1,
+            duration: 300,
+        });
+        changeNavBar(e.index);
+    }
 }
 
 function changeNavBar(index) {
-	if (index < 0 || index >= tabStacks.length) {
-		return;
-	}
+    if (index < 0 || index >= tabStacks.length) {
+        return;
+    }
 
-	if (!_.isNull($[tabStacks[index].winId].getViewById('title'))) {
-        $[tabStacks[index].winId].getViewById('title').text = tabStacks[index].winName;
-        $[tabStacks[index].winId].getViewById('btnRight').visible = tabStacks[index].hasSettingsEnabled;
+    if (!_.isNull($[tabStacks[index].winId].getViewById('title'))) {
+        // $[tabStacks[index].winId].getViewById('title').text = tabStacks[index].winName;
+        // $[tabStacks[index].winId].getViewById('btnRight').visible =
+        //     tabStacks[index].hasSettingsEnabled;
     }
 }
 
@@ -147,115 +160,124 @@ Ti.App.addEventListener('pause', enableBackground);
 Ti.App.addEventListener('resume', resume);
 
 function enableBackground() {
-	Ti.App.Properties.setBool('background', true);
-	Alloy.Globals.background = true;
+    Ti.App.Properties.setBool('background', true);
+    Alloy.Globals.background = true;
 
-	timeSinceBackgroundApp = Alloy.Globals.moment();
+    timeSinceBackgroundApp = Alloy.Globals.moment();
 }
 
 function resume(e) {
-	Ti.API.info('App is resuming from the background');
+    Ti.API.info('App is resuming from the background');
 
-	if (isDiffTimePassed()) {
-		refreshNewsAndSchedule();
-	}
+    if (isDiffTimePassed()) {
+        refreshNewsAndSchedule();
+    }
 
-	if (checkValidUser()) {
-		Alloy.Globals.Api.me(function (response) {
-			if (response.success) {
-				return;
-			}
+    if (checkValidUser()) {
+        Alloy.Globals.Api.me(function (response) {
+            if (response.success) {
+                return;
+            }
 
-			closeSession();
-			setTimeout(() => {
-				Alloy.Globals.showMessage('Por motivos de seguridad, se ha cerrado la sesión.');
-			}, 1000);
-		});
-	}
+            closeSession();
+            setTimeout(() => {
+                Alloy.Globals.showMessage(
+                    'Por motivos de seguridad, se ha cerrado la sesión.'
+                );
+            }, 1000);
+        });
+    }
 
-	if (OS_IOS) {
-		Ti.UI.iOS.setAppBadge(0);
-	}
+    if (OS_IOS) {
+        Ti.UI.iOS.setAppBadge(0);
+    }
 
-	if (OS_ANDROID) {
-		const data = {
-			'tag': fcm.lastData.tag || null,
-			'url': fcm.lastData.url || null,
-			'data': fcm.lastData.data || null,
-		};
-		notification(data);
-	}
+    if (OS_ANDROID) {
+        const data = {
+            tag: fcm.lastData.tag || null,
+            url: fcm.lastData.url || null,
+            data: fcm.lastData.data || null,
+        };
+        notification(data);
+    }
 }
 
 function checkValidUser() {
-	return (Alloy.Globals.online) && (Alloy.Globals.token !== null) && (!Alloy.Globals.guest);
+    return (
+        Alloy.Globals.online &&
+        Alloy.Globals.token !== null &&
+        !Alloy.Globals.guest
+    );
 }
 
 function isDiffTimePassed() {
-	return Alloy.Globals.moment().diff(timeSinceBackgroundApp, 'minutes') >= time2RefreshNewsAndEventsInMinutes;
+    return (
+        Alloy.Globals.moment().diff(timeSinceBackgroundApp, 'minutes') >=
+        time2RefreshNewsAndEventsInMinutes
+    );
 }
 
 Alloy.Globals.events.on('handle_notification', notification);
 
 function refreshNewsAndSchedule() {
-	Alloy.Globals.events.trigger('refreshNews');
-	Alloy.Globals.events.trigger('refreshSchedule');
+    Alloy.Globals.events.trigger('refreshNews');
+    Alloy.Globals.events.trigger('refreshSchedule');
 }
 
 function notification(data) {
-	let title = 'Evento';
+    let title = 'Evento';
 
-	if ((!data) && (_.isNull(data.tag))) {
-		return;
-	}
+    if (!data && _.isNull(data.tag)) {
+        return;
+    }
 
-	switch (data.tag) {
-		case 'news':
-			title = 'Noticia';
-		case 'events':
-			refreshNewsAndSchedule();
-			Alloy.createController('webviewWin', {
-				url: data.url,
-				title
-			})
-			.getView()
-			.open();
-			break;
+    switch (data.tag) {
+        case 'news':
+            title = 'Noticia';
+        case 'events':
+            refreshNewsAndSchedule();
+            Alloy.createController('webviewWin', {
+                url: data.url,
+                title,
+            })
+                .getView()
+                .open();
+            break;
 
-		case `sector_${data.sectorId}`:
-			if (Alloy.Globals.background) {
-				Ti.App.Properties.setBool('background', false);
-				Alloy.Globals.background = false;
-				Alloy.Globals.events.trigger('demmand_notification', data);
-			} else {
-				Alloy.Globals.showMessage(data.body, 'Centro de Negocio');
-			}
-			break;
-	}
-	if (OS_ANDROID) {
-		Alloy.Globals.androidDataPush = null;
-	}
+        case `sector_${data.sectorId}`:
+            if (Alloy.Globals.background) {
+                Ti.App.Properties.setBool('background', false);
+                Alloy.Globals.background = false;
+                Alloy.Globals.events.trigger('demmand_notification', data);
+            } else {
+                Alloy.Globals.showMessage(data.body, 'Centro de Negocio');
+            }
+            break;
+    }
+    if (OS_ANDROID) {
+        Alloy.Globals.androidDataPush = null;
+    }
 }
 
 function closeSession() {
-	Ti.App.removeEventListener('pause', enableBackground);
-	Ti.App.removeEventListener('resume', resume);
-	removeProperties();
-	require("/dao/database").reset("variable");
-	$.tabGroup.close();
-	Alloy.createController("/login/login", {
-		closeApp: true
-	})
-	.getView()
-	.open();
+    Ti.App.removeEventListener('pause', enableBackground);
+    Ti.App.removeEventListener('resume', resume);
+    removeProperties();
+    require('/dao/database').reset('variable');
+    $.tabGroup.close();
+    Alloy.createController('/login/login', {
+        closeApp: true,
+    })
+        .getView()
+        .open();
 }
 Alloy.Globals.events.on('closeSession', closeSession);
 
 function removeProperties() {
-	Ti.App.Properties.removeProperty('');
-	Ti.App.Properties.setBool('guest', true);
-	Alloy.Globals.guest = true;
-	Alloy.Globals.token = Alloy.Globals.user = null;
-	Ti.App.Properties.setBool("isConnected", false);
-	Alloy.Globals.events.off();
+    Ti.App.Properties.removeProperty('');
+    Ti.App.Properties.setBool('guest', true);
+    Alloy.Globals.guest = true;
+    Alloy.Globals.token = Alloy.Globals.user = null;
+    Ti.App.Properties.setBool('isConnected', false);
+    Alloy.Globals.events.off();
 }

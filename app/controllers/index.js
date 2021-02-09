060e838d5b477896b28d2f56b716b89c7597c8c7
Jose Cerrejon <ulysess@gmail.com>
2021-02-09 10:20:05 +0100

    👌 IMPROVE: index only redirect to dashboard WIP

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -1,8 +1,9 @@
 (function constructor(args) {
+    Alloy.createController('dashboard').getView().open();
     // Ti.App.Properties.setBool('isConnected', false);
-    if (Ti.App.Properties.getBool('isConnected')) {
-        Alloy.createController('dashboard').getView().open();
-    } else {
-        Alloy.createController('login/login', args).getView().open();
-    }
+    // if (Ti.App.Properties.getBool('isConnected')) {
+    //     Alloy.createController('dashboard').getView().open();
+    // } else {
+    //     Alloy.createController('login/login', args).getView().open();
+    // }
 })($.args);

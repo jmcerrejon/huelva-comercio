dccd23962bd3fab658e103fd3877718721be0723
Jose Cerrejon <ulysess@gmail.com>
2021-02-17 13:51:06 +0100

    📦 NEW: Feature Covenants and Services

---
+++ app/controllers/affiliates/services.js
@@ -0,0 +1,9 @@
+(function constructor(args) {
+    Alloy.Collections[args.model || 'covenants'].fetch({
+        success: (res) => {},
+    });
+})($.args);
+
+function doOpenURL(params) {
+    Ti.Platform.openURL(params.source.itemLink);
+}

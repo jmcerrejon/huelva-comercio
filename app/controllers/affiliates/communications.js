0ece7a6a89bffb9460fcabdce3b4aa974ce5714a
Jose Cerrejon <ulysess@gmail.com>
2021-02-18 12:33:40 +0100

    🐛 FIX: HeaderView goes inside Listview

--- app/controllers/affiliates/communications.js
+++ app/controllers/affiliates/communications.js
@@ -1,3 +1,73 @@
+let currentPage = 1;
+let postsPerPage = 10;
+let query = '';
+const SCROLLABLEVIEW_HEIGHT = 175;
+const collectionName = 'newsletters';
+
+// console.log(JSON.stringify($.args, null, 2));
+
+(function constructor(args) {
+    Alloy.Collections[collectionName].fetch({
+        page: currentPage++,
+        query,
+        success: (res) => {
+            Alloy.Globals.loading.hide();
+            postsPerPage = res.length;
+            if (!$.is) return;
+            $.is.init($.listView);
+            $.is.setOptions({
+                msgTap: '',
+                msgDone: '',
+                msgError: 'Toca para intentar de nuevo...',
+            });
+        },
+    });
+})($.args);
+
+function reset() {
+    console.log('Refresh with reset()');
+    Alloy.Globals.loading.show('Cargando...');
+    // renderNotFoundTemplate();
+    currentPage = 1;
+    Alloy.Collections[collectionName].fetch({
+        page: currentPage,
+        query: '',
+        success: (resource) => {
+            Alloy.Globals.loading.hide();
+            $.refreshListView.endRefreshing();
+            currentPage++;
+            if (!$.is) return;
+            $.is.show();
+            $.is.mark();
+        },
+    });
+}
+
+function myLoader(element) {
+    console.log('Inf Scroll with myLoader()');
+    Alloy.Collections[collectionName].fetch({
+        page: currentPage,
+        query: '',
+        add: true,
+        success: function (collection) {
+            currentPage++;
+            if (_.isUndefined(element)) {
+                return;
+            }
+            collection.length % postsPerPage == 0
+                ? element.success()
+                : element.done();
+        },
+        error: function (col) {
+            Alloy.Globals.showMessage(
+                'No se pueden mostrar. Inténtelo mas tarde.'
+            );
+        },
+    });
+}
+
+// Original functions
+
 function doOpenServices(e) {
     openInsideNavWindow({
         model: 'services',

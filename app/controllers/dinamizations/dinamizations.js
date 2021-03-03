bf9544564d5a6bb516349a5554764b3f17eada83
Jose Cerrejon <ulysess@gmail.com>
2021-03-03 11:05:26 +0100

    📦 NEW: Dinamization feature

---
+++ app/controllers/dinamizations/dinamizations.js
@@ -0,0 +1,29 @@
+(function constructor(args) {
+    if (!Alloy.Globals.guest) {
+        $.vwGuest.width = $.vwGuest.height = 0;
+        $.scrDinamizations.width = '100%';
+        $.scrDinamizations.height = '85%';
+    }
+
+    Alloy.Collections['dinamizations'].fetch({
+        success: (res) => {},
+    });
+})($.args);
+
+function doOpenSign(params) {
+    Alloy.createController('login/login')
+        .getView()
+        .open(
+            OS_IOS
+                ? {
+                      modal: true,
+                      modalTransitionStyle:
+                          Ti.UI.iOS.MODAL_TRANSITION_STYLE_COVER_VERTICAL,
+                  }
+                : {}
+        );
+}
+
+function doOpenURL(params) {
+    Ti.Platform.openURL(params.source.itemLink);
+}

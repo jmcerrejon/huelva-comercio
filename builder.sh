b9041c35879c9d1305c0775c77dc02753937ad0a
Jose Cerrejon <ulysess@gmail.com>
2021-03-11 16:24:12 +0100

    👌 IMPROVE:  Refactor code. Change PP_UUID

--- builder.sh
+++ builder.sh
@@ -3,25 +3,25 @@
 PROJECT_DIR="/Users/ulysess/Documents/Appcelerator_Studio_Workspace/foe-ahora"
 
 # iOS
-ios () {
+ios() {
     echo "Remember: If you change minor version (max.min.path, for example 1.0.0 -> 1.1.0), it takes 24 hours for submit review. Path version are inmediately submitted."
-	DISTRIBUTION_NAME="Apple Distribution: SOPORTTEC SERVICIOS INFORMATICOS INTEGRADOS PARA EMPRESAS SL (WDS48L3DS9)"
-	PP_UUID="53c2f1ac-aa8e-4977-b476-ba593eed3002"
+    DISTRIBUTION_NAME="Apple Distribution: SOPORTTEC SERVICIOS INFORMATICOS INTEGRADOS PARA EMPRESAS SL (WDS48L3DS9)"
+    PP_UUID="20bb2139-0cd2-4a65-8195-fc4b5421e4c0"
 
-	appc run -p ios -T dist-appstore --log-level info --project-dir "$PROJECT_DIR" --distribution-name "$DISTRIBUTION_NAME" --pp-uuid "$PP_UUID"
+    appc run -p ios -T dist-appstore --log-level info --project-dir "$PROJECT_DIR" --distribution-name "$DISTRIBUTION_NAME" --pp-uuid "$PP_UUID"
 }
 
 # ANDROID
-android () {
-	KEYSTORE="$PROJECT_DIR/cert/android/com.sopportec.foeahora.jks"
-	OUTPUT_DIR="$PROJECT_DIR/dist"
-	appc run -p android -T dist-playstore --log-level info --project-dir "/Users/ulysess/Documents/Appcelerator_Studio_Workspace/foe-ahora" --output-dir "$OUTPUT_DIR" --keystore "$KEYSTORE" --alias foeahora --store-password SoporFOE42!
+android() {
+    KEYSTORE="$PROJECT_DIR/cert/android/com.sopportec.foeahora.jks"
+    OUTPUT_DIR="$PROJECT_DIR/dist"
+    appc run -p android -T dist-playstore --log-level info --project-dir "/Users/ulysess/Documents/Appcelerator_Studio_Workspace/foe-ahora" --output-dir "$OUTPUT_DIR" --keystore "$KEYSTORE" --alias foeahora --store-password SoporFOE42!
 }
 
 clear
 read -p "Select device for package: iOS or Android? (ios/and): " option
 case "$option" in
-	ios ) ios ;;
-	and ) android ;;
-	*) ios ;;
+ios) ios ;;
+and) android ;;
+*) ios ;;
 esac

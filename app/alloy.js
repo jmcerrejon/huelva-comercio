75e7753bfc6824d7bf243e929b3cc299468590b7
Jose Cerrejon <ulysess@gmail.com>
2021-03-12 13:40:55 +0100

    🐛 FIX: Navbar position on iOS with Notch or not

--- app/alloy.js
+++ app/alloy.js
@@ -81,7 +81,7 @@ if (OS_IOS) {
 // Push notifications
 require('notifications')();
 
-Alloy.Globals.top = OS_IOS ? (Alloy.Globals.Device.isiPhoneX ? 40 : 20) : 0;
+Alloy.Globals.top = OS_IOS ? (Alloy.Globals.Device.isiPhoneX ? 20 : 0) : 0;
 Alloy.Globals.footerHeight = OS_IOS
     ? Alloy.Globals.Device.isiPhoneX
         ? 74

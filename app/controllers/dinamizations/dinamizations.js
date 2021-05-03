4c5e18f4987f4859e9d841804d69009f4df2451b
Jose Cerrejon <ulysess@gmail.com>
2021-05-03 20:26:09 +0200

    👌 IMPROVE: Dinamization view

--- app/controllers/dinamizations/dinamizations.js
+++ app/controllers/dinamizations/dinamizations.js
@@ -1,8 +1,81 @@
+let currentPage = 1;
+let POSTS_PER_PAGE = 15;
+
 (function constructor() {
     if (!Alloy.Globals.guest) {
-        Alloy.Collections['dinamizations'].fetch();
+        Alloy.Collections['dinamizations'].fetch({
+            page: currentPage++,
+            success: (res) => {
+                POSTS_PER_PAGE = res.length;
+                if (POSTS_PER_PAGE === 0) {
+                    renderNotFoundTemplate(res.length);
+                    return;
+                }
+                $.is.init($.listView);
+                $.is.setOptions({
+                    msgTap: '',
+                    msgDone: '',
+                    msgError: 'Toca para intentar de nuevo...',
+                });
+            },
+        });
     }
 })();
+
+function myLoader(element) {
+    Alloy.Collections['dinamizations'].fetch({
+        page: currentPage,
+        add: true,
+        success: function (collection) {
+            currentPage++;
+            if (_.isUndefined(element)) {
+                return;
+            }
+            collection.length % POSTS_PER_PAGE == 0
+                ? element.success()
+                : element.done();
+        },
+        error: function () {
+            Alloy.Globals.showMessage(
+                'No se pueden mostrar. Inténtelo mas tarde.'
+            );
+        },
+    });
+}
+
+function reset() {
+    if (!Ti.Network.online) {
+        $.refreshListView.endRefreshing();
+        Alloy.Globals.showMessage(
+            'Intente primero obtener conexión a internet.'
+        );
+        return;
+    }
+    Alloy.Globals.loading.show('Cargando ofertas...');
+    renderNotFoundTemplate();
+    currentPage = 1;
+    Alloy.Collections['dinamizations'].fetch({
+        page: currentPage,
+        success: () => {
+            Alloy.Globals.loading.hide();
+            $.refreshListView.endRefreshing();
+
+            $.is.show();
+            $.is.mark();
+            currentPage++;
+        },
+    });
+}
+
+function renderNotFoundTemplate(sizeData) {
+    let item = $.listView.sections[0].items[0];
+    // item.properties = {
+    //     height: sizeData === 0 ? '250' : 0,
+    // };
+    // $.listView.sections[0].updateItemAt(0, item);
+    // Remove Infinity Scroll when show not items availables on search (Remember do $.is.show() later)
+    $.is.hide();
+}
 
 function doOpenSign() {
     Alloy.createController('login/login')
@@ -18,6 +91,23 @@ function doOpenSign() {
         );
 }
 
-function doOpenURL(params) {
-    Ti.Platform.openURL(params.source.itemLink);
+function doOpenPost(e) {
+    const item = e.section.getItemAt(e.itemIndex);
+
+    if (
+        !_.isUndefined(item.vwDinamizations.url) &&
+        !_.isNull(item.vwDinamizations.url)
+    ) {
+        !item.vwDinamizations || openURL(item.vwDinamizations.url);
+    }
+}
+
+function openURL(url, title = 'Huelva Comercio (web)', share) {
+    Alloy.createController('webviewWin', {
+        title,
+        url,
+        share,
+    })
+        .getView()
+        .open();
 }

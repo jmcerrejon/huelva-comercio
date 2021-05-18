0a19aa1fea4d8822d306f58096ee0b57dfec3593
Jose Cerrejon <ulysess@gmail.com>
2021-05-18 20:16:57 +0200

    👌 IMPROVE: Remove comments

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -1,9 +1,3 @@
-(function constructor(args) {
+(function constructor() {
     Alloy.createController('dashboard').getView().open();
-    // Ti.App.Properties.setBool('isConnected', false);
-    // if (Ti.App.Properties.getBool('isConnected')) {
-    //     Alloy.createController('dashboard').getView().open();
-    // } else {
-    //     Alloy.createController('login/login', args).getView().open();
-    // }
-})($.args);
+})();
